008b50284e026cab86b53403481ce6b2f7c356bc
ahmetkasap <aahmetkasap@gmail.com>
2025-08-11 22:01:42 +0300

    asd

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -76,7 +76,7 @@ export const createBlog = async (req, res) => {
     // HTML içeriğini temizle
     const sanitizedContent = sanitizeHtml(content, {
       allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'u', 's', 'span', 'code', 'hr', 'br', 'div',
         'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'iframe' ],
       allowedAttributes: {
         'a': [ 'href', 'name', 'target' ],
@@ -113,7 +113,7 @@ export const updateBlog = async (req, res) => {
     // HTML içeriğini temizle
     const sanitizedContent = sanitizeHtml(content, {
       allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'u', 's', 'span', 'code', 'hr', 'br', 'div',
         'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'iframe' ],
       allowedAttributes: {
         'a': [ 'href', 'name', 'target' ],
